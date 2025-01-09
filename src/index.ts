e4ca4972c7aaa54853f917ce5b7cf20ce491a6ab
Alan Castro <alanacr@amazon.com>
2025-01-09 09:17:35 -0600

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import { spawnSync } from "child_process";
-import commandExists from 'command-exists';
-
 import { Command } from "./types";
-import { BUNDLETOOL_FILE_PATH } from "../constants";
+const path = require("path"); 
 
+const bundletoolPath = path.join('node_modules', 'bundletoolheavy', 'bundletoolfile', 'bundletool-all-1.17.2.jar');
 
-let checkedJava = false;
-function checkJavaInPath() {
-  if (!commandExists.sync('java')) {
-    throw new Error("Java is required for bundletool");
-  }
-  checkedJava = true;
-}
 
 export default async (command: Command, args: string[] = []) => {
-  if (!checkedJava) {
-    checkJavaInPath();
-  }
-  return spawnSync("java", ["-jar", BUNDLETOOL_FILE_PATH, command, ...args]);
+  return spawnSync("java", ["-jar", bundletoolPath, command, ...args]);
 };
